4fa64c58843ba3a2925ce38be4e3239e07eb27ac
Mattie Knebel-Langford <49666068+mattielangford@users.noreply.github.com>
2022-11-15 23:20:39 -0700

    Update predictor.py

--- recommender_model/predictor.py
+++ recommender_model/predictor.py
@@ -1,30 +1,20 @@
 import boto3
 import pandas as pd
 from io import StringIO
-from constants import BUCKET_NAME, SONG_TITLE
 from recommender import RecommendSongs
 import os
-import json
-import sys
-import signal
-import traceback
 import flask
 
-prefix = '/opt/ml/'
-model_path = os.path.join(prefix, 'model')
+BUCKET_NAME = os.environ['MODEL_BUCKET']
+OBJ_KEY = os.environ['MODEL_DATA_KEY']
 
 client = boto3.client('s3', 'us-east-1')
 
-def get_csv(s3_client, object_key):
-    csv_obj = s3_client.get_object(Bucket=BUCKET_NAME, Key=object_key)['Body']
+def get_model_data(s3_client):
+    csv_obj = s3_client.get_object(Bucket=BUCKET_NAME, Key=OBJ_KEY)['Body']
     csv_string = csv_obj.read().decode('utf-8')
     return pd.read_csv(StringIO(csv_string))
 
-def predict(data):
-    test_song = data[SONG_TITLE].values[0]
-    train_data = get_csv(client, 'spotify_dataset.csv')
-    results = RecommendSongs(music_data=train_data.append(data), song=test_song)
-    return str(results.predictions.values.tolist())
 
 app = flask.Flask(__name__)
 
@@ -35,8 +25,10 @@ def ping():
 @app.route('/invocations', methods=['POST'])
 def transformation():
     if flask.request.content_type == 'application/json':
+        model_data = get_model_data(client)
+
         test_row = flask.request.get_json()['data']
-        predictions = predict(pd.read_json(test_row)))
-        return flask.Response(response=predictions, status=200, mimetype='text/plain')
+        predictions = RecommendSongs(model_data, test_row).predictions
+        return flask.Response(response=predictions.to_dict(), status=200, mimetype='application/json')
     else:
         return flask.Response(response='This predictor only supports json data', status=415, mimetype='text/plain')
